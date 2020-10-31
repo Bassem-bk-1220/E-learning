118594114c7d7fa6eb58afd7ea2dcb8c841ffbb3
Belkahla Bassem <61603951+Bassem-bk-1220@users.noreply.github.com>
2020-10-31 21:36:37 +0200

    Add files via upload

---
+++ inc/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Learning</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/bootstra.css" />
+	<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+	<header>
+		<div class="top">
+			<div class="container">
+				<div class="row">
+				<div style="width:50px;height:50px"> <img src="image/elearning.jpg" style="width:150px;height:60px;"/></div>
+					
+					<div class="col-md-6 navi" style="margin-left:25%;">
+						<ul>
+							<li><a href="index.html">HOME</a></li>
+							<div class="dropdown">
+                            <li><a href="">COURSE</a></li>
+                            <div class="dropdown-content">
+							    <a href="courshtml.html">courshtml</a>
+								<a href="courscss.html">courscss</a>
+                                <a href="coursjava.html">coursjava</a>
+                                <a href="coursphp.html">coursphp</a>
+                            </div>
+                            </div>
+							<div class="dropdown">
+                            <li><a href="">QCM</a></li>
+                            <div class="dropdown-content">
+							    <a href="qcmhtml.html">QCMhtml</a>
+								<a href="qcmcss.html">QCMCSS</a>
+                                <a href="qcmjava.html">QCMjava</a>
+                                <a href="qcmphp.html">QCMphp</a>
+                            </div>
+                            </div>
+							<li><a href="contact.html">CONTACT</a></li>
+							<li><a href="login.html">Log-in</a></li>
+						</ul>
+					</div>
+										
+			        </div>
+				     </div>
+				 </div>
+	</header>
